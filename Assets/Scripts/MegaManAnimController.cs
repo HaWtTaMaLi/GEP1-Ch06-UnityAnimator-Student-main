e5f5a6428cee02fef3de12219422f5d3cbd08a70
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-02-26 18:43:43 -0400

    Update MegaMan Anim Controller

--- Assets/Scripts/MegaManAnimController.cs
+++ Assets/Scripts/MegaManAnimController.cs
@@ -5,12 +5,29 @@ public class MegaManAnimController : MonoBehaviour
 {
     public Animator megaManAnimController;
 
-    
+    public int isIdleHash = Animator.StringToHash("isIdle");
 
     public void Start()
     {
         megaManAnimController = GetComponent<Animator>();
     }
 
-     
+     public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        float direction = input.x; 
+
+        //isIdle
+        if (direction == 0)
+        {
+            megaManAnimController.SetBool(isIdleHash, true);
+        }
+        else
+        {
+            megaManAnimController.SetBool(isIdleHash, false);
+        }
+
+        // Flip the character based on movement direction
+
+    }
 }
