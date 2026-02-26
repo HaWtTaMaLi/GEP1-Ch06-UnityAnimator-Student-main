ad18bdb610905569a4849623ff044a3a1ad62991
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-02-26 15:26:33 -0400

    Cube Rotating

--- Assets/Scripts/CubeSpinnerAnimController.cs
+++ Assets/Scripts/CubeSpinnerAnimController.cs
@@ -1,11 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CubeSpinnerAnimController : MonoBehaviour
 {
     public Animator cubeAnimController;
 
+    private int isRotatingHash = Animator.StringToHash("isRotating");
+
     public void Start()
     {
         cubeAnimController = GetComponent<Animator>();
+    }
+
+    public void OnSpin(InputAction.CallbackContext context)
+    {
+        Debug.Log("Toggled Spin");
+
+        if(context.performed && cubeAnimController.GetBool(isRotatingHash) == true)
+        {
+            // animator = spin cube
+            cubeAnimController.SetBool(isRotatingHash, false);
+        }
+
+        else if (context.performed && cubeAnimController.GetBool(isRotatingHash) == false)
+        {
+            // animator = stop spinning cube
+            cubeAnimController.SetBool(isRotatingHash, true);
+        }
     }
 }
