01e10a04644e90b76f3f5b502dcbad3ded70227a
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2026-02-26 18:31:47 -0400

    push

--- Assets/Scripts/MegaManAnimController.cs
+++ Assets/Scripts/MegaManAnimController.cs
@@ -1,6 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MegaManAnimController : MonoBehaviour
 {
+    public Animator megaManAnimController;
 
+    
+
+    public void Start()
+    {
+        megaManAnimController = GetComponent<Animator>();
+    }
+
+     
 }
